95406724985cfbf54791c8adca7c615853e38c4d
k1rh4 <k1rh4@k1rh4ui-MacBook-Pro.local>
2018-08-21 13:07:21 +0900

    Add Auto install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+cd ../..
+cd llvm/tools/clang/tools
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+cd ../../../..
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk co
+mkdir build
+cd build
+cmake -G "Unix Makefiles" ../
+make
+
